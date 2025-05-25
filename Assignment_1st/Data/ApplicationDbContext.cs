0db83f053374f2a53bbd82192c7b9713479fc969
kanakWeb <kanakwebdev@gmail.com>
2025-05-26 00:36:05 +0600

    Add project files.

---
+++ Assignment_1st/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Assignment_1st.Models;    
+
+namespace Assignment_1st.Data
+{
+    public class ApplicationDbContext
+    {
+    }
+}
